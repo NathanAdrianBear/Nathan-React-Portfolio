894181b2bef70aa7fb2f3be65219323f20b21324
Nathan Adrian <nathan@beargroup.com>
2025-06-27 15:04:18 -0500

    remove unneeded code

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,16 +1,4 @@
 import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom'
-import Logo from '../../components/Logo'
-import {
-  faLinkedin,
-  faGithub,
-} from '@fortawesome/free-brands-svg-icons'
-import {
-  faHome,
-  faUser,
-  faEnvelope
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Sidebar from '../../components/Sidebar'
 
 const Navigation = () => {
